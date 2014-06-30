1e40bd117154ce21ea97c94135d0b35159ad3c9a
thanakorn <stylus_l@hotmail.com>
2014-06-30 23:14:57 +0700

    - Ask for current package - Recommend packages

---
+++ src/response_generator.py
@@ -0,0 +1,47 @@
+__author__ = 'thanakorn'
+
+from database import DatabaseManager
+from question import Question
+
+
+class ResponseGenerator(object):
+
+    def __init__(self):
+        self._db = DatabaseManager()
+
+    def generate_response_message(self, question, customer):
+        if question.information == 'package':
+            package = self._db.find_user_package(customer['phone_number'])
+            # Recommend package
+            if question.verb in ['know', 'recommend', 'have'] and question.adjective in ['internet', 'calling', 'calling and internet']:
+                recommend_packages = self._db.find_package_by_category(question.adjective, package['payment'])
+                response = 'we recommend %d packages for you. ' % (recommend_packages.count())
+                for p in recommend_packages:
+                    response += '%s. ' % (p['name'])
+                    if package['payment'] == 'postpaid':
+                        response += 'maximum calling time is %d minutes, maximum speed internet data is %d gigabytes and monthly fee is %d baht. ' % (p['calling_time'], p['max_speed_data'], p['fee'])
+                    elif package['payment'] == 'prepaid':
+                        response += 'internal calling rate is %.2f baht per minute, external calling rate is %.2f baht per minute, and registration fee is %d baht. ' % (p['internal_calling_rate'], p['external_calling_rate'], p['registration_fee'])
+                return response
+            # Ask for current package
+            elif question.question in ['what', 'which'] and (question.adjective is None or question.adjective == 'current'):
+                if package['payment'] == 'postpaid':
+                    response = 'your current package is %s. maximum call time is %d minutes. maximum internet speed data is %d gigabytes. monthly fee is equal to %d baht.' % (package['name'], package['calling_time'], package['max_speed_data'], package['fee'])
+                else:
+                    response = 'your current package is %s. internal calling rate is %.2f baht per minute. external calling rate is %.2f baht per minute. registration fee is %d baht.' % (package['name'], package['internal_calling_rate'], package['external_calling_rate'], package['registration_fee'])
+                return response
+            # Otherwise
+            else:
+                return 'sorry i do not understand your question'
+
+        elif question.information in ['bill', 'money', 'balance', 'account']:
+            # Get user's package
+            package = self._db.find_user_package(customer['phone_number'])
+            if package['payment'] == 'postpaid':
+                if question.question == 'how much' and question.verb == 'pay':
+                    response = 'your service fee this month is %s baht.' % (package['fee'])
+                return response
+            elif package['payment'] == 'prepaid':
+                if question.question == 'how much' or question.adjective == 'current':
+                    pass
+

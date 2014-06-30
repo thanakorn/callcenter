eda76738428fbf5f3583e758f19c4ba54af061a7
thanakorn <stylus_l@hotmail.com>
2014-06-30 11:25:49 +0700

    Question format

---
+++ src/question.py
@@ -0,0 +1,9 @@
+__author__ = 'thanakorn'
+
+class Question(object):
+
+    def __init__(self, question, verb, adjective, information):
+        self.question = question
+        self.verb = verb
+        self.adjective = adjective
+        self.information = information
